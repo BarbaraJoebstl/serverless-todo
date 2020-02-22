2f2fd275c195f0c00bd912f103314ad3d311d221
Barbara Jöbstl <Barbara.Joebstl@mercedes-benz.io>
2020-02-22 13:21:49 +0100

    feat: create and read todos via serverless.yaml

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,36 @@
 import 'source-map-support/register'
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
+import * as uuid from 'uuid'
+import { TodoItem } from '../../models/TodoItem'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  console.log('Processing event: ', event)
+  const todoId = uuid.v4()
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+  const parsedBody = JSON.parse(event.body)
+  
+  const newTodo: TodoItem = {
+    id: todoId,
+    createdAt: Date.now(),
+    ...parsedBody
+  }
+
+  await docClient.put({
+    TableName: todosTable,
+    Item: newTodo
+  }).promise()
+
+  return {
+    statusCode: 201,
+    headers: {'Access-Control-Allow-Origin': '*'
+  },
+    body: JSON.stringify({
+      newTodo
+    })
+  }
 }
